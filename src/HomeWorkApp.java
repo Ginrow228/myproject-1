077e857f7e72f2607bd6c98646596ee878f00b37
Ruslan Karimov <badginrow@gmail.com>
2024-11-12 16:45:59 +0500

    Создал новый метод compareNumbers для домашнего задания 3, задача 4

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,9 +1,10 @@
 public class HomeWorkApp {
 
     public static void main(String[] args){
-         //printThreeWords();
-         //checkSumSign(-10,-5);
-        //printColor(-1);
+//         printThreeWords();
+//         checkSumSign(-10,-5);
+//         printColor(-1);
+//         compareNumbers(5,6);
     }
 
     static void printThreeWords(){// метод который выводит 3 слова в табличном виде
@@ -32,6 +33,14 @@ public class HomeWorkApp {
             System.out.println("Жёлтый"); // если value в пределах от 0 (исключительно) 100 (включительно) вывести слово Жёлтый
         } else {
             System.out.println("Зелёный"); // если value больше 100 вывести слово Зелёный
+        }
+    }
+
+    static void compareNumbers(int a, int b){ // метод который определяет больше || равно или меньше значение
+        if (a >= b){
+            System.out.println("a >= b"); // если значение a больше или равно b, то вывести на консоль результат
+        } else {
+            System.out.println("a < b"); // если значение a меньше значения b, то вывести на консоль результат
         }
     }
 
