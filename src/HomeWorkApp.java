3c7a65a98f8c8ba02c9fecf4c271a3369795fb90
Ruslan Karimov <badginrow@gmail.com>
2024-11-12 16:43:12 +0500

    Создал новый метод checkSumSign для домашнего задания 3, задача 3

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -3,6 +3,7 @@ public class HomeWorkApp {
     public static void main(String[] args){
          //printThreeWords();
          //checkSumSign(-10,-5);
+        //printColor(-1);
     }
 
     static void printThreeWords(){// метод который выводит 3 слова в табличном виде
@@ -21,6 +22,16 @@ public class HomeWorkApp {
             System.out.println("Сумма положительна!"); // если сумма чисел больше или равна 0 вывести Сумма положительна
         } else {
             System.out.println("Сумма отрицательна"); // в противном случае вывести Сумма отрицательна
+        }
+    }
+
+    static void printColor(int value){ // метод который выводит цвет, в зависимости от переданного значения
+        if(value <= 0){
+            System.out.println("Красный"); // если value меньше или ровно 0 вывести слово Красный
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый"); // если value в пределах от 0 (исключительно) 100 (включительно) вывести слово Жёлтый
+        } else {
+            System.out.println("Зелёный"); // если value больше 100 вывести слово Зелёный
         }
     }
 
