cfac753805a25974eb2fbe5bf7f3f4dc48b6cbb3
Ruslan Karimov <badginrow@gmail.com>
2024-11-12 16:40:10 +0500

    Создал новый метод checkSumSign для домашнего задания 3, задача 2

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,7 +1,8 @@
 public class HomeWorkApp {
 
     public static void main(String[] args){
-       // printThreeWords();
+         //printThreeWords();
+         //checkSumSign(-10,-5);
     }
 
     static void printThreeWords(){// метод который выводит 3 слова в табличном виде
@@ -12,6 +13,15 @@ public class HomeWorkApp {
                 Apple
                 """
         );
+    }
+
+    static void checkSumSign(int a, int b){ // метод который считает положительна или отрицательна сумма
+        int c = a + b;
+        if (c >= 0){
+            System.out.println("Сумма положительна!"); // если сумма чисел больше или равна 0 вывести Сумма положительна
+        } else {
+            System.out.println("Сумма отрицательна"); // в противном случае вывести Сумма отрицательна
+        }
     }
 
 }
