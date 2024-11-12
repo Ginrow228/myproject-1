2db41bcd199997c24838d8fd4a261f1a50f52b36
Ruslan Karimov <badginrow@gmail.com>
2024-11-12 16:31:11 +0500

    Создал новый метод printThreeWords для домашнего задания 3, задача 1

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,7 +1,17 @@
 public class HomeWorkApp {
 
     public static void main(String[] args){
+       // printThreeWords();
+    }
 
+    static void printThreeWords(){// метод который выводит 3 слова в табличном виде
+        System.out.println(
+                """
+                Orange
+                Banana
+                Apple
+                """
+        );
     }
 
 }
