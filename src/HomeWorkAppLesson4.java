e3b627d621c03621079577708d2b222c14aa4200
Ruslan Karimov <badginrow@gmail.com>
2024-11-20 19:02:34 +0500

    Создал новый метод fillArray для домашнего задания 4, задача 2

--- src/HomeWorkAppLesson4.java
+++ src/HomeWorkAppLesson4.java
@@ -2,11 +2,20 @@ public class HomeWorkAppLesson4 {
     public static void main(String[] args){
 //        int[] arr = {0, 1, 0, 1, 0};
 //        replace1With0(arr);
+//        fillArray(100);
 
     }
     static void replace1With0(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] > 0 ? 0 : 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void fillArray(int size){
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + 1;
             System.out.print(arr[i] + " ");
         }
     }
