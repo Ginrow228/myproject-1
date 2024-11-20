db6dc79db3b4f994796a1c3b5b1466df31fe6e45
Ruslan Karimov <badginrow@gmail.com>
2024-11-21 00:18:30 +0500

    Выравнил код в линию

--- src/HomeWorkAppLesson4.java
+++ src/HomeWorkAppLesson4.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class HomeWorkAppLesson4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        int[] arr = {0, 1, 0, 1, 0};
 //        replace1With0(arr);
 //        fillArray(250);
@@ -9,29 +9,29 @@ public class HomeWorkAppLesson4 {
 //        arr = multiplyArray(arr);
 //        System.out.println(Arrays.toString(arr));
 //        swapArray(7, 5);
-/*          int[][] arr = {
+/*        int[][] arr = {
                   {0, 0, 0, 0, 0},
                   {0, 0, 0, 0, 0},
                   {0, 0, 0, 0, 0},
                   {0, 0, 0, 0, 0},
                   {0, 0, 0, 0, 0}
           };
-            matrixDiagonal(arr); */
-//          int[] arr = {83, 33, 6, 65, 97, 2, 1, 0, -5, -7};
-//          findMinMax(arr);
-//            int[] arr = {1, 1, 1, 2, 1, 5};
-//            System.out.println(checkSum(arr));
-
+          matrixDiagonal(arr); */
+//        int[] arr = {83, 33, 6, 65, 97, 2, 1, 0, -5, -7};
+//        findMinMax(arr);
+//        int[] arr = {1, 1, 1, 2, 1, 5};
+//        System.out.println(checkSum(arr));
 
     }
-    static void replace1With0(int[] arr){
+
+    static void replace1With0(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] > 0 ? 0 : 1;
             System.out.print(arr[i] + " ");
         }
     }
 
-    static void fillArray(int size){
+    static void fillArray(int size) {
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = i + 1;
@@ -39,16 +39,16 @@ public class HomeWorkAppLesson4 {
         }
     }
 
-    static int[] multiplyArray(int[] arr){
+    static int[] multiplyArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < 6){
+            if (arr[i] < 6) {
                 arr[i] *= 2;
             }
         }
         return arr;
     }
 
-    static int[] swapArray(int len, int initialValue){
+    static int[] swapArray(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < len; i++) {
             arr[i] = initialValue;
@@ -56,10 +56,10 @@ public class HomeWorkAppLesson4 {
         return arr;
     }
 
-    static void matrixDiagonal(int[][] arr){
+    static void matrixDiagonal(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if(i == j || i + j == arr[i].length - 1){
+                if (i == j || i + j == arr[i].length - 1) {
                     arr[i][j] = 1;
                 }
                 System.out.print(arr[i][j] + " ");
@@ -68,13 +68,13 @@ public class HomeWorkAppLesson4 {
         }
     }
 
-    static void findMinMax(int[] arr){
+    static void findMinMax(int[] arr) {
         int min = arr[0];
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < min){
+            if (arr[i] < min) {
                 min = arr[i];
-            }else if(arr[i] > max){
+            } else if (arr[i] > max) {
                 max = arr[i];
             }
         }
@@ -82,8 +82,8 @@ public class HomeWorkAppLesson4 {
         System.out.println("Maximum: " + max);
     }
 
-    static boolean checkSum(int[] arr){
-        if(arr.length < 2){
+    static boolean checkSum(int[] arr) {
+        if (arr.length < 2) {
             return false;
         }
         int totalSum = 0;
@@ -94,7 +94,7 @@ public class HomeWorkAppLesson4 {
         for (int i = 0; i < arr.length; i++) {
             leftSum += arr[i];
             int rightSum = totalSum - leftSum;
-            if(leftSum == rightSum){
+            if (leftSum == rightSum) {
                 return true;
             }
         }
