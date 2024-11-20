5edfd6731dc168ed8fd2d0c3598b512a67252db5
Ruslan Karimov <badginrow@gmail.com>
2024-11-20 18:57:45 +0500

    Создал новый метод replace1With0 для домашнего задания 4, задача 1

--- src/HomeWorkAppLesson4.java
+++ src/HomeWorkAppLesson4.java
@@ -1,5 +1,13 @@
 public class HomeWorkAppLesson4 {
     public static void main(String[] args){
+//        int[] arr = {0, 1, 0, 1, 0};
+//        replace1With0(arr);
 
+    }
+    static void replace1With0(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] > 0 ? 0 : 1;
+            System.out.print(arr[i] + " ");
+        }
     }
 }
