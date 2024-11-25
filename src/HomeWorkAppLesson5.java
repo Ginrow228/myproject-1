342074adb5ccab6ffd12dfde737c8698386d6b24
Ginrow228 <badginrow@gmail.com>
2024-11-25 22:24:00 +0500

    Создал новые методы для домашнего задания 5, задача 3

--- src/HomeWorkAppLesson5.java
+++ src/HomeWorkAppLesson5.java
@@ -9,6 +9,7 @@ public class HomeWorkAppLesson5 {
          System.out.println(Arrays.toString(arr));
          System.out.println(searchIndex(arr,4));
          System.out.println(lengthList(arr)); */
+        /** Блок очереди
         arr = queue(7);
         addInQueue(arr, 2);
         addInQueue(arr, 4);
@@ -20,6 +21,24 @@ public class HomeWorkAppLesson5 {
         addInQueue(arr, 20);
         System.out.println(Arrays.toString(arr));
         getInQueue(arr);
+        System.out.println(Arrays.toString(arr)); */
+        arr = init(10);
+        System.out.println(Arrays.toString(arr));
+        addToHead(arr, 5);
+        addToHead(arr, 15);
+        addToHead(arr, 25);
+        addToHead(arr, 35);
+        addToHead(arr, 45);
+        addToTail(arr, 55);
+        addToTail(arr, 65);
+        addToTail(arr, 75);
+        addToTail(arr, 85);
+        addToTail(arr, 95);
+        addToHead(arr, 105);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(getFromHead(arr));
+        System.out.println(Arrays.toString(arr));
+        System.out.println(getFromTail(arr));
         System.out.println(Arrays.toString(arr));
     }
 
@@ -48,22 +67,24 @@ public class HomeWorkAppLesson5 {
         return arr.length;
     } */
 
-
+    /*
     static int[] queue(int sizeQueue) {
         if (sizeQueue <= 0) {
             return new int[0];
         }
         return new int[sizeQueue + 1];
-    }
+    } */
 
+    /*
     static void addInQueue(int[] arr, int val) {
-        if (arr.length <= 0 || arr.length - 1 == arr[0]) { // проверка на выход за пределы массива
+        if (arr.length <= 0 || arr.length - 1 == arr[0]) {
             return;
         }
         ++arr[0];
         arr[arr[0]] = val;
-    }
+    } */
 
+    /*
     static int getInQueue(int[] arr) {
         if (arr.length == 0 || arr[0] == 0) {
             return -1;
@@ -78,5 +99,60 @@ public class HomeWorkAppLesson5 {
         }
         --arr[0];
         return firstValue;
+    }*/
+
+    static int[] init(int size) {
+        if (size <= 0) {
+            return new int[0];
+        }
+        int[] arr = new int[size + 2];
+        arr[0] = 2;
+        arr[1] = arr.length - 1;
+        return arr;
+    }
+
+    static void addToHead(int[] arr, int value) {
+        if (arr.length <= 2 || arr[0] >= arr[1]) {
+            return;
+        }
+        arr[arr[0]] = value;
+        ++arr[0];
+    }
+
+    static int getFromHead(int[] arr) {
+        if (arr[0] == 2) {
+            return -1;
+        }
+        int value = arr[2];
+        for (int i = 3; i < arr[0]; i++) {
+            arr[i-1] = arr[i];
+            if (arr[0] == i+1) {
+                arr[i] = 0;
+                break;
+            }
+        }
+        --arr[0];
+        return value;
+    }
+
+    static void addToTail(int[] arr, int value) {
+        if (arr.length <= 2 || arr[0] + arr[1] == arr.length +1) {
+            return;
+        }
+        arr[arr[1]] = value;
+        --arr[1];
+    }
+
+    static int getFromTail(int[] arr) {
+        int maxIndex = arr.length - 1;
+        if (arr[1] == maxIndex) {
+            return -1;
+        }
+        int value = arr[maxIndex];
+        for (int i = maxIndex; i > arr[1]; i--) {
+            arr[i] = arr[i-1];
+        }
+        ++arr[1];
+        return value;
     }
 }
