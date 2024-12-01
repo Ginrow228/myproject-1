5681163d704598b1df0d7fdc407016155faee7df
Ruslan Karimov <badginrow@gmail.com>
2024-12-01 18:51:08 +0500

    Создал новую ветку archive-old-classes для переноса старых классов

--- src/old/HomeWorkAppLesson3.java
+++ src/old/HomeWorkAppLesson3.java
@@ -1,3 +1,5 @@
+package old;
+
 public class HomeWorkAppLesson3 {
 //    public static void main(String[] args) {
 //         printThreeWords();
--- src/old/HomeWorkAppLesson6.java
+++ src/old/HomeWorkAppLesson6.java
@@ -1,3 +1,5 @@
+package old;
+
 import java.util.Arrays;
 
 public class HomeWorkAppLesson6 {
@@ -6,14 +8,18 @@ public class HomeWorkAppLesson6 {
             int[] target = {};
 
             int[] source2 = {1, 2, 3, 4};
-            int[] target2 = {5, 6, 7, 8, 9};
+            int[] target2 = {5, 6, 7};
 
             arrayCopy(source, target);
             arrayCopy(source2, target2);
 
-            int[] forSort = {5, 7, 1, 2, 9, 6, 4, 8, 3};
-            shakeSort(forSort);
-            System.out.println(Arrays.toString(forSort));
+//            int[] forSort = {5, 7, 1, 2, 9, 6, 4, 8, 3};
+//            shakeSort(forSort);
+//            System.out.println(Arrays.toString(forSort));
+
+            int[] test = {1, 1, 1, 1, 1};
+            shakeSort(test);
+            System.out.println(Arrays.toString(test));
     }
 
     static void arrayCopy(int[] source, int[] target){
@@ -33,25 +39,36 @@ public class HomeWorkAppLesson6 {
     static void shakeSort(int[] array){
         int left = 0;
         int right = array.length -1;
+        boolean swapped = true;
         while (left < right) {
+            swapped = false; // задаем false, т.к еще никаких проходов не было
             for (int j = 0; j < right; j++) {
                 if(array[j] > array[j + 1]){
                     var temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped){
+                break;
+            }
             right--;
+
+            swapped = false;
             for (int j = right; j > 0; j--) {
                 if(array[j] < array[j - 1]){
                     var temp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
+                    swapped = true;
                 }
             }
             left++;
+            if(!swapped){
+                break;
+            }
         }
     }
-
 }
 
