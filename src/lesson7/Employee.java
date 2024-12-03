717fbb6b9c9ce8f779ad24c62846bcc7d82c250c
Ruslan Karimov <badginrow@gmail.com>
2024-12-03 19:14:09 +0500

    Создал класс Employee, Car, SameName, SameName для домашнего задания 7

---
+++ src/lesson7/Employee.java
@@ -0,0 +1,38 @@
+package lesson7;
+
+public class Employee {
+    String info;
+    String jobTitle;
+    String email;
+    String numberPhone;
+    int age;
+
+    public Employee(String info, String jobTitle, String email, String numberPhone, int age) {
+        this.info = info;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.numberPhone = numberPhone;
+        this.age = age;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
