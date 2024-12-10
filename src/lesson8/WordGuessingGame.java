7b469a668096f06f3d9e83e453e21fbc63dd8608
Ruslan Karimov <badginrow@gmail.com>
2024-12-10 14:02:36 +0500

    Создал новый класс WordGuessingGame с новым методом startGame в котором реализовал игру "Угадай слово" для домашнего задания 8, задача 5

--- src/lesson8/WordGuessingGame.java
+++ src/lesson8/WordGuessingGame.java
@@ -2,6 +2,7 @@ package lesson8;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
 public class WordGuessingGame {
 
     String[] words = {
@@ -16,25 +17,22 @@ public class WordGuessingGame {
         Random rand = new Random();
         String randomWord = words[rand.nextInt(words.length)];
         String playerWord;
-        String hint = "";
+        char[] hintArr = new char[15];
+        Arrays.fill(hintArr, '#');
 
         while(true){
             System.out.print("Enter word: ");
             playerWord = scanner.nextLine();
-
             if(playerWord.equals(randomWord)){
                 break;
             } else {
                 int minLen = randomWord.length() < playerWord.length() ? randomWord.length() : playerWord.length();
                 for (int i = 0; i < minLen; i++) {
                     if(randomWord.charAt(i) == playerWord.charAt(i)){
-                        hint += randomWord.charAt(i);
-                    } else {
-                        while(hint.length() < 15){
-                            hint += "#";
-                        }
+                        hintArr[i] = randomWord.charAt(i);
                     }
                 }
+                String hint = new String(hintArr);
                 System.out.println("Hint to the hidden word: " + hint);
             }
         }
