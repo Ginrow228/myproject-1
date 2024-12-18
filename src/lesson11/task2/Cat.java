21605824bec1bad8851e476bd866a7a1e3a3b369
Ruslan Karimov <badginrow@gmail.com>
2024-12-18 20:13:04 +0500

    Создал 3 класса, которые представляют из себя участников, в них реализовал методы run и jump

---
+++ src/lesson11/task2/Cat.java
@@ -0,0 +1,20 @@
+package lesson11.task2;
+
+public class Cat implements Member{
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run(int distance) {
+        System.out.println(name + " смог пробежать дистанцию в " + distance);
+    }
+
+    @Override
+    public void jump(int height) {
+        System.out.println(name + " хватило сил перепрыгнуть препятствие");
+    }
+}
---
+++ src/lesson11/task2/Human.java
@@ -0,0 +1,20 @@
+package lesson11.task2;
+
+public class Human implements Member {
+
+    private String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run(int distance) {
+        System.out.println(name + " смог пробежать дистанцию в " + distance);
+    }
+
+    @Override
+    public void jump(int height) {
+        System.out.println(name + " хватило сил перепрыгнуть препятствие");
+    }
+}
