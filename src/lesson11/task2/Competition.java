a85b24c7350aec06714c15f926b90b5c5ec825bb
Ruslan Karimov <badginrow@gmail.com>
2024-12-18 20:46:15 +0500

    В методе start класса Competition, обновлена проверка, на возможность участников пройти задание

--- src/lesson11/task2/Competition.java
+++ src/lesson11/task2/Competition.java
@@ -16,9 +16,21 @@ public class Competition {
 
             for (int j = 0; j < obstacles.length; j++) {
                 Obstacle obstacle = obstacles[j];
+                int parameter = obstacle.getParameter();
+                if(obstacle instanceof Treadmill){
+                    if(!member.canRun(parameter)){
+                        System.out.println(member + " не справляется с препятствием длиной " + parameter);
+                        break;
+                    }
+                } else if (obstacle instanceof Wall) {
+                    if(!member.canJump(parameter)){
+                        System.out.println(member + " не в состоянии перепрыгнуть препятствие");
+                        break;
+                    }
+                }
                 obstacle.overcome(member);
             }
-            System.out.println(member + " закончил соревнование");
+            System.out.println(member + " завершает соревнование");
         }
     }
 }
