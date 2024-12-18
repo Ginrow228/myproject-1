c097fb538da91905510b9eaa5ddb8088b5217e94
Ruslan Karimov <badginrow@gmail.com>
2024-12-18 20:54:01 +0500

    Была добавлена проверка на прохождение всех препятствий

--- src/lesson11/task2/Competition.java
+++ src/lesson11/task2/Competition.java
@@ -13,22 +13,28 @@ public class Competition {
         for (int i = 0; i < members.length; i++) {
             Member member = members[i];
             System.out.println(member + " начинает соревнование");
+            boolean passedAllObstacles = true;
 
             for (int j = 0; j < obstacles.length; j++) {
                 Obstacle obstacle = obstacles[j];
                 int parameter = obstacle.getParameter();
                 if(obstacle instanceof Treadmill){
                     if(!member.canRun(parameter)){
-                        System.out.println(member + " не справляется с препятствием длиной " + parameter);
+                        System.out.println(member + " не справляется с препятствием длиной в " + parameter + "м.");
+                        passedAllObstacles = false;
                         break;
                     }
                 } else if (obstacle instanceof Wall) {
                     if(!member.canJump(parameter)){
-                        System.out.println(member + " не в состоянии перепрыгнуть препятствие");
+                        System.out.println(member + " не в состоянии перепрыгнуть препятствие длиной в " + parameter + "м.");
+                        passedAllObstacles = false;
                         break;
                     }
                 }
                 obstacle.overcome(member);
+            }
+            if(passedAllObstacles){
+                System.out.println(member + " прошел все препятствия");
             }
             System.out.println(member + " завершает соревнование");
         }
