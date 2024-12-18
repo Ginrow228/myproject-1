22c5580b47503e1ff420f67ea9d0636c97f0ee58
Ruslan Karimov <badginrow@gmail.com>
2024-12-18 18:30:09 +0500

    В абстрактный класс Animal, были добавлены геттеры и метод который считает количество животных

--- src/lesson10/Animal.java
+++ src/lesson10/Animal.java
@@ -1,14 +1,30 @@
 package lesson10;
 
 public abstract class Animal {
-
-    protected static int totalAnimal = 0;
-    protected static int dogs = 0;
-    protected static int cats = 0;
-
+    protected static int totalAnimal;
+    protected static int dogs;
+    protected static int cats;
 
     public Animal() {
         totalAnimal++;
+    }
+
+    public static int getTotalAnimal() {
+        return totalAnimal;
+    }
+
+    public static int getDogs() {
+        return dogs;
+    }
+
+    public static int getCats() {
+        return cats;
+    }
+
+    public static void numberOfAnimals(){
+        System.out.println("Общее количество животных: " + getTotalAnimal());
+        System.out.println("Количество собак: " + getDogs());
+        System.out.println("Количество кошек: " + getCats());
     }
 
     public abstract void run(double distance);
