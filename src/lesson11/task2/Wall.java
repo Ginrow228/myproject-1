b8aa154145d69613ad073986b5831be06bad80c7
Ruslan Karimov <badginrow@gmail.com>
2024-12-18 20:18:29 +0500

    Создал два класса Treadmill и Wall, которые подразумевают препятствия, в которых реализовал метод overcome

---
+++ src/lesson11/task2/Wall.java
@@ -0,0 +1,15 @@
+package lesson11.task2;
+
+public class Wall implements Obstacle{
+
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void overcome(Member member) {
+        member.jump(height);
+    }
+}
