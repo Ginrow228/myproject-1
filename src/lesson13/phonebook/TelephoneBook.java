895760f8b916dd72ca4bd64551125e75757e76dc
Ruslan Karimov <badginrow@gmail.com>
2024-12-26 20:05:58 +0500

    Создал класс TelephoneBook, который представляет из себя телефонную книгу

---
+++ src/lesson13/phonebook/TelephoneBook.java
@@ -0,0 +1,15 @@
+package lesson13.phonebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TelephoneBook {
+
+    private List<Record> records;
+
+    public TelephoneBook() {
+        this.records = new ArrayList<>();
+    }
+
+
+}
