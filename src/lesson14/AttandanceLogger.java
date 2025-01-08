730737d6df47188430769b2df4e0ac302901ae04
Ruslan Karimov <badginrow@gmail.com>
2025-01-09 00:37:42 +0500

    Обновил код с учетом добавления класса для информации посещаемости, а так же обновил метод mostPopularHour

--- src/lesson14/AttandanceLogger.java
+++ src/lesson14/AttandanceLogger.java
@@ -6,8 +6,7 @@ import java.time.LocalTime;
 import java.util.*;
 
 public class AttandanceLogger {
-
-    private List<String[]> log;
+    private List<Visit> log;
     private Map<String, Integer> visitFrequency;
 
     public AttandanceLogger() {
@@ -15,56 +14,53 @@ public class AttandanceLogger {
         this.visitFrequency = new HashMap<>();
     }
 
-    public void registerVisit(String userID, String timestamp){
-        log.add(new String[]{userID, timestamp});
+    public void registerVisit(String userID, String timestamp) {
+        Visit newVisit = new Visit(userID, timestamp);
+        log.add(newVisit);
         visitFrequency.put(userID, visitFrequency.getOrDefault(userID, 0) + 1);
 
     }
 
-    public Map<String, Integer> frequencyOfVisits(){
+    public Map<String, Integer> frequencyOfVisits() {
         return new HashMap<>(visitFrequency);
     }
 
-    public void printAllVisits(){
-        for (String[] record : log) {
-            System.out.println(record[0] + " " + record[1]);
+    public void printAllVisits() {
+        for (Visit visit : log) {
+            System.out.println(visit);
         }
     }
 
-    public void printVisitFrequencies(){
+    public void printVisitFrequencies() {
         Map<String, Integer> frequencyMap = frequencyOfVisits();
-        for(String userID : frequencyMap.keySet()){
+        for (String userID : frequencyMap.keySet()) {
             Integer visitCounter = frequencyMap.get(userID);
             System.out.println(userID + ": " + visitCounter);
         }
     }
 
-    public String mostPopularHour(){
+    public String mostPopularHour() {
         Map<String, Integer> hourFrequency = new TreeMap<>();
-        for(String[] record : log){
-            String visitTime = record[1];
-            String hour = visitTime.substring(0, 2);
-            hourFrequency.put(hour, hourFrequency.getOrDefault(hour, 0) + 1);
-        }
-
         int maxVisits = 0;
-        for(String hour : hourFrequency.keySet()){
-            int visitCounter = hourFrequency.get(hour);
-            if(visitCounter > maxVisits){
-                maxVisits = visitCounter;
+
+        for (Visit visit : log) {
+            String visitTime = visit.getTimestamp();
+            String hour = visitTime.substring(0, 2);
+            int count = hourFrequency.getOrDefault(hour, 0) + 1;
+            hourFrequency.put(hour, count);
+            if (count > maxVisits) {
+                maxVisits = count;
             }
         }
         String mostPopularHour = "";
-        for (String hour : hourFrequency.keySet()){
-            if(hourFrequency.get(hour) == maxVisits){
-                if(!mostPopularHour.isEmpty()){
+        for (String hour : hourFrequency.keySet()) {
+            if (hourFrequency.get(hour) == maxVisits) {
+                if (!mostPopularHour.isEmpty()) {
                     mostPopularHour += ", ";
                 }
                 mostPopularHour += hour + " : 00";
             }
         }
-
         return mostPopularHour;
     }
-
 }
