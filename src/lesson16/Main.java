3d78da98d415f9fc72d074c6244e6f57071a05ee
Ginrow228 <badginrow@gmail.com>
2025-01-13 21:53:40 +0500

    Обновил класс Main, с учетом добавления bfs-алгоритма

--- src/lesson16/Main.java
+++ src/lesson16/Main.java
@@ -1,16 +1,19 @@
-package lesson16;
+package src.lesson16;
 
 public class Main {
     public static void main(String[] args){
         int[][] map = {
-                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1},
                 {1, 1, 0, 0, 0},
                 {0, 0, 0, 1, 1},
                 {0, 1, 0, 1, 1},
         };
 
-        Voyager voyager = new DfsVoyager();
-        int islands = voyager.lookupIslands(map);
-        System.out.println(islands);
+//        DfsVoyager voyager = new DfsVoyager();
+//        int islandsByDfs = voyager.lookupIslands(map);
+//        System.out.println(islandsByDfs);
+        BfsVoyager voyager2 = new BfsVoyager();
+        int islandsByBfs = voyager2.lookupIslands(map);
+        System.out.println(islandsByBfs);
     }
 }
