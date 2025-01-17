d32837316552c1ca8fd8fc04bf9672398871b82f
Ruslan Karimov <badginrow@gmail.com>
2025-01-17 20:38:53 +0500

    Создал класс Box, который представляет из себя ящик с фруктами

---
+++ src/lesson17/fruits/Box.java
@@ -0,0 +1,13 @@
+package lesson17.fruits;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> fruits;
+
+    public Box() {
+        this.fruits = new ArrayList<>();
+    }
+
+}
