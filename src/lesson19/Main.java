00e3f50590dc9cd31286b71023781c62d33ff14e
Ruslan Karimov <badginrow@gmail.com>
2025-02-03 19:13:01 +0500

    В классе Main протестировал программу

---
+++ src/lesson19/Main.java
@@ -0,0 +1,20 @@
+package lesson19;
+
+import java.io.File;
+
+public class Main {
+     public static void main(String[] args) {
+         File file = new File("file.md");
+         FileLoggerConfiguration configuration = new FileLoggerConfiguration(
+                 LoggingLevel.DEBUG,
+                 file,
+                 1000
+         );
+
+         Logger logger = new FileLogger(configuration);
+         logger.debug("hello");
+         logger.info("info message");
+
+
+    }
+}
