920b844e5dadf7514ca1ff063ed10f1b666642b0
Ruslan Karimov <badginrow@gmail.com>
2025-02-03 19:21:34 +0500

    Проверил FileLoggerConfigurationLoader в Main

--- src/lesson19/Main.java
+++ src/lesson19/Main.java
@@ -4,17 +4,25 @@ import java.io.File;
 
 public class Main {
      public static void main(String[] args) {
-         File file = new File("file.md");
-         FileLoggerConfiguration configuration = new FileLoggerConfiguration(
-                 LoggingLevel.DEBUG,
-                 file,
-                 1000
-         );
+//         File file = new File("file.md");
+//         FileLoggerConfiguration configuration = new FileLoggerConfiguration(
+//                 LoggingLevel.DEBUG,
+//                 file,
+//                 1000
+//         );
+//
+//         Logger logger = new FileLogger(configuration);
+//         logger.debug("hello");
+//         logger.info("info message");
 
-         Logger logger = new FileLogger(configuration);
-         logger.debug("hello");
-         logger.info("info message");
+         File configuratonFile = new File("log.properties");
+         FileLoggerConfigurationLoader loader = new FileLoggerConfigurationLoader();
+         FileLoggerConfiguration configuration1 = loader.load(configuratonFile);
 
+         Logger logger1 = new FileLogger(configuration1);
+
+         logger1.info("info message");
+         logger1.debug("debug message"); // не выведется
 
     }
 }
