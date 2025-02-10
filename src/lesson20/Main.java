dc89889e0c598c2ada508ca736ed4db97807635c
Ruslan Karimov <badginrow@gmail.com>
2025-02-10 16:21:06 +0500

    Проверил методы read в Main

--- src/lesson20/Main.java
+++ src/lesson20/Main.java
@@ -7,15 +7,35 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Path storagePath = Paths.get("storage");
+//
+//        FileStorage fileStorage = new FileStorage(storagePath);
+//
+//        // Добавление в хранилище
+//        Path fileStore = Paths.get("file3");
+//        fileStorage.put("namespace1", "needed_file.txt", fileStore);
+//
+//        // Получение
+//        Path file = fileStorage.get("namespace1", "needed_file.txt");
 
-        FileStorage fileStorage = new FileStorage(storagePath);
+        FileStorageReader fileStorageReader = new FileStorageReader(storagePath);
+        try {
+            byte[] file1 = fileStorageReader.read("namespace1", "needed_file.txt");
+            System.out.println(new String(file1));
+        } catch (RuntimeException e){
+            e.printStackTrace();
+        }
 
-        // Добавление в хранилище
-        Path fileStore = Paths.get("file3");
-        fileStorage.put("namespace1", "needed_file.txt", fileStore);
+        System.out.println("===================");
 
-        // Получение
-        Path file = fileStorage.get("namespace1", "needed_file.txt");
+        try {
+            int chunkSize = 50;
+            List<byte[]> chunks = fileStorageReader.read("namespace1", "needed_file.txt", chunkSize);
+            for (byte[] chunk : chunks){
+                System.out.println(new String(chunk));
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
 
     }
 }
