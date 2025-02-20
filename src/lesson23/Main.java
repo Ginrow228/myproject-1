f52b3f5282f48f4a751d24ca2b782cef0ec2a2a5
Ruslan Karimov <badginrow@gmail.com>
2025-02-20 20:05:30 +0500

    Реализовал статический класс TestRunner с методом start, который предназначен для выполнения тестовых классов

---
+++ src/lesson23/Main.java
@@ -0,0 +1,54 @@
+package lesson23;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static class TestRunner {
+        public static void start(Class<?> aClass) throws Exception {
+            Method[] methods = aClass.getDeclaredMethods();
+            Method beforeSuitemethod = null;
+            Method afterSuiteMethod = null;
+            List<Method> testMethods = new ArrayList<>();
+
+            for(Method method : methods){
+                if(method.isAnnotationPresent(BeforeSuite.class)){
+                    if(beforeSuitemethod != null){
+                        throw new IllegalStateException("@BeforeSuite метод уже существует");
+                    }
+                    beforeSuitemethod = method;
+                } else if (method.isAnnotationPresent(AfterSuite.class)){
+                    if(afterSuiteMethod != null) {
+                        throw new IllegalStateException("@AfterSuite метож уже существует");
+                    }
+                    afterSuiteMethod = method;
+                } else if (method.isAnnotationPresent(Test.class)){
+                    testMethods.add(method);
+                }
+            }
+            if(beforeSuitemethod != null){
+                beforeSuitemethod.setAccessible(true);
+                beforeSuitemethod.invoke(null);
+            }
+
+            testMethods.sort(Comparator.comparingInt(m -> m.getAnnotation(Test.class).order()));
+
+            for(Method method : testMethods){
+                System.out.println("Запускаем тест:" + method.getName());
+                method.setAccessible(true);
+                method.invoke(null);
+            }
+
+            if(afterSuiteMethod != null){
+                afterSuiteMethod.setAccessible(true);
+                afterSuiteMethod.invoke(null);
+            }
+        }
+    }
+}
