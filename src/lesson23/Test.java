e3713744c16ed29124a2fa94200105658bc388bc
Ruslan Karimov <badginrow@gmail.com>
2025-02-20 19:42:29 +0500

    Реализовал три аннотации, которые могут приминяться только к методам

---
+++ src/lesson23/Test.java
@@ -0,0 +1,12 @@
+package lesson23;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+    int order() default Integer.MAX_VALUE;
+}
