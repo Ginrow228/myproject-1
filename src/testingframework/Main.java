2d51af2e636f465be34fd9b03c35f1c19e71fff0
Ruslan Karimov <badginrow@gmail.com>
2025-02-23 23:59:43 +0500

    Запустил в Main

--- src/testingframework/Main.java
+++ src/testingframework/Main.java
@@ -1,9 +1,17 @@
 package testingframework;
 
+import testingframework.printers.ConsolePrinter;
+import testingframework.printers.FilePrinter;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        new TestAutomationRunner(List.of(ForExample.class)).run();
+        new ExtendedTestAutomationRunner(
+                List.of(ForExample.class),
+               new HashSet<>(Set.of(new ConsolePrinter(), new FilePrinter("test_results.md")))
+                ).run();
     }
 }
