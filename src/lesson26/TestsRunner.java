5f2b18df367f48a07fcc83bc4bff1316de051d44
Ruslan Karimov <badginrow@gmail.com>
2025-03-02 23:42:29 +0500

    Запустил выполнение тестового класса

---
+++ src/lesson26/TestsRunner.java
@@ -0,0 +1,29 @@
+package lesson26;
+
+import org.junit.platform.engine.discovery.ClassNameFilter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import java.io.PrintWriter;
+
+public class TestsRunner {
+    public static void main(String[] args) {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectPackage("lesson26")) // Укажите свой пакет
+                .filters(ClassNameFilter.includeClassNamePatterns(".*Test"))
+                .build();
+
+        Launcher launcher = LauncherFactory.create();
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        listener.getSummary().printTo(new PrintWriter(System.out));
+    }
+}
+
+
