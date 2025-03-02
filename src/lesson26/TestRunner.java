8a9769025f8f4f8081ed3781cba47242514459f5
Ruslan Karimov <badginrow@gmail.com>
2025-03-03 00:04:36 +0500

    Запустил выполнение тестовых классов

---
+++ src/lesson26/TestRunner.java
@@ -0,0 +1,25 @@
+package lesson26;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import java.io.PrintWriter;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectPackage("lesson26"))
+                .build();
+
+        Launcher launcher = LauncherFactory.create();
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        listener.getSummary().printTo(new PrintWriter(System.out));
+    }
+}
